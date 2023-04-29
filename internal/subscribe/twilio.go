90efb13076810b3a8c3f830781b00ea52b523564
Oliver Benns <oliverbenns@gmail.com>
2023-04-29 15:23:20 +0100

    Request validation

--- internal/subscribe/twilio.go
+++ internal/subscribe/twilio.go
@@ -56,10 +56,21 @@ func (s twilioSubscriber) Subscribe() (<-chan string, <-chan error) {
 }
 
 func (s twilioSubscriber) onWebhook(w http.ResponseWriter, r *http.Request) {
-	// @TODO: validate req
+	err := s.validateRequest(r)
+	if err != nil {
+		s.errs <- fmt.Errorf("could not validate request: %w", err)
+		return
+	}
+
 	msg, err := s.parseRequest(r)
 	if err != nil {
 		s.errs <- fmt.Errorf("could not parse request: %w", err)
+		return
+	}
+
+	err = s.validateMsg(msg)
+	if err != nil {
+		s.errs <- fmt.Errorf("msg failed validation: %w", err)
 		return
 	}
 
@@ -83,6 +94,15 @@ type Msg struct {
 	WaId             string
 }
 
+func (s twilioSubscriber) validateRequest(r *http.Request) error {
+	// @TODO: validate req signature with auth token.
+	// https://www.twilio.com/docs/usage/webhooks/webhooks-security
+	// We need to know the webhook url ahead of time
+	// in order to do this. Difficult when ngrok tunnelling or using
+	// generated cloud run urls that happen after deploy.
+	return nil
+}
+
 func (s twilioSubscriber) parseRequest(r *http.Request) (*Msg, error) {
 	err := r.ParseForm()
 	if err != nil {
@@ -97,6 +117,21 @@ func (s twilioSubscriber) parseRequest(r *http.Request) (*Msg, error) {
 	}
 
 	return msg, nil
+}
+
+func (s twilioSubscriber) validateMsg(msg *Msg) error {
+	to := fmt.Sprintf("whatsapp:%s", s.sendTo)
+	from := fmt.Sprintf("whatsapp:%s", s.sendFrom)
+
+	if to != msg.To {
+		return fmt.Errorf("invalid 'to' param: %s", msg.To)
+	}
+
+	if from != msg.From {
+		return fmt.Errorf("invalid 'from' param: %s", msg.From)
+	}
+
+	return nil
 }
 
 var _ Subscriber = twilioSubscriber{}
